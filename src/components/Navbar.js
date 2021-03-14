b58963e987f2a14da819a10b2f0a95fe2bee9a77
SoyYuyin <emtzhd@gmail.com>
2021-03-14 14:44:05 -0500

    Chagned a tags to Links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,10 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>The Mini React Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/create'}>Create</Link>
+        {/* <a href="/">Home</a>
+        <a href="/create">New Blog</a> */}
       </div>
     </nav>
   );
